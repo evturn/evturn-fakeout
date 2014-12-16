ad879a407dd92bd95ccd148b594554f2eae761e8
Evan <evturn@gmail.com>
2014-12-16 14:37:25 -0500

    fourth

--- lib/evturn_fakeout/funny_sentence.rb
+++ lib/evturn_fakeout/funny_sentence.rb
@@ -8,9 +8,42 @@ module EvturnFakeout
       noun = Faker::Hacker.noun
       verb = Faker::Hacker.verb
       ingverb = Faker::Hacker.ingverb
-      name = Faker::Name.name
+      name = ["D'Marcus Williums",
+              "T.J. Juckson",
+              "T'varisuness King",
+              "Tyroil Smoochie-Wallace",
+              "D'Squarius Green, Jr.",
+              "Ibrahim Moizoos",
+              "Jackmerius Tacktheritrix",
+              "D'Isiah T. Billings-Clyde",
+              "D'Jasper Probincrux III",
+              "Leoz Maxwell Jilliumz",
+              "Javaris Jamar Javarison-Lamar",
+              "Davoin Shower-Handel",
+              "Hingle McCringleberry",
+              "L'Carpetron Dookmarriot",
+              "J'Dinkalage Morgoone",
+              "Xmus Jaxon Flaxon-Waxon",
+              "Saggitariutt Jefferspin",
+              "D'Glester Hardunkichud",
+              "Swirvithan L'Goodling-Splatt",
+              "Quatro Quatro",
+              "Ozamataz Buckshank",
+              "Beezer Twelve Washingbeard",
+              "Shakiraquan T.G.I.F. Carter",
+              "X-Wing @Aliciousness",
+              "Sequester Grundelplith M.D.",
+              "Scoish Velociraptor Maloish",
+              "T.J. A.J. R.J. Backslashinfourth V",
+              "Eeeee Eeeeeeeee",
+              "Donkey Teeth",
+              "Wolfgang Fuck",
+              "Torque (Construction Noise) Lewith",
+              "The Player Formerly Known as Mousecop",
+              "Dan Smith"
+              ].sample
 
-      "#{name} and Wolfgang Fuck #{verb} the #{ingverb} #{noun}. Wolfgang Fuck specializes in #{ingverb} and on #{noun} while eating copious amount of #{name}'s #{ingverb} #{noun}"
+      "#{name} #{verb}s the #{ingverb} #{noun}. But what you didn't know is #{name} is also #{ingverb} my #{noun}."
     
     end
 
